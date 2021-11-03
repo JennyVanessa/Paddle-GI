7087ff21e4b9cf92299619a2a60b88e5e654d4df
JennyVanessa <44646718+JennyVanessa@users.noreply.github.com>
2021-11-03 10:30:22 +0800

    Update dataset.py

--- generative-inpainting/datatest/dataset.py
+++ generative-inpainting/datatest/dataset.py
@@ -1,12 +1,9 @@
 import sys
-#import torch.utils.data as data
 from os import listdir
 from utils.tools import default_loader, is_image_file, normalize
 import os
 import numpy as np
 import paddle
-
-#import torchvision.transforms as transforms
 
 import paddle.vision.transforms as transforms
 
@@ -41,9 +38,6 @@ class Dataset(Dataset):
 
         img = transforms.ToTensor()(img)  # turn the image to a tensor
 
-        #img = img.numpy()
-        #img=np.around(img,4)
-        #img=paddle.to_tensor(img)
         
         img = normalize(img)
         img=img.numpy()
@@ -64,15 +58,12 @@ class Dataset(Dataset):
             No class is a subdirectory of another.
         """
         if sys.version_info >= (3, 5):
-            # Faster and available in Python 3.5 and above
-            #classes = [d.name for d in os.scandir(dir) if d.is_dir()]
             classes = [d for d in os.listdir(dir)]
         else:
             classes = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]
         classes.sort()
-        #class_to_idx = {classes[i]: i for i in range(len(classes))}
         samples = []
-        #for target in sorted(class_to_idx.keys()):
+        
         for target in sorted(classes):
             d = os.path.join(dir, target)
             samples.append(d)
